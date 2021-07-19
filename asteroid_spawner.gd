f62ff555b2d3b8f70c0261577bb826c9663af0fb
RichoM <richi.moran@gmail.com>
2021-07-19 13:13:09 -0300

    Changed asteroid spawner to remove all asteroids and start over when it has spawned enough

--- asteroid_spawner.gd
+++ asteroid_spawner.gd
@@ -2,11 +2,10 @@ extends Spatial
 
 export var root : NodePath
 export var camera : NodePath
-export var max_asteroids = 7
+export var max_asteroids = 5
 
 var asteroids = [preload("res://asteroid_0.tscn"), preload("res://asteroid_1.tscn")]
-var counter = 0
-
+var spawned = []
 
 func _ready():
 	var timer = $timer
@@ -15,10 +14,16 @@ func _ready():
 	call_deferred("spawn_new_asteroid")
 
 func spawn_new_asteroid():
-	if counter < max_asteroids:
-		var index = posmod(counter, asteroids.size())
-		var asteroid = asteroids[index].instance() as Asteroid
-		asteroid.camera = get_node(camera).get_path()
-		get_node(root).add_child(asteroid)
-		asteroid.global_transform.origin = global_transform.origin
-		counter += 1
+	var counter = spawned.size()
+	if counter >= max_asteroids:
+		for asteroid in spawned:
+			(asteroid as Asteroid).remove_flag = true
+		spawned.clear()
+		
+	print(OS.get_time())
+	var index = posmod(counter, asteroids.size())
+	var asteroid = asteroids[index].instance() as Asteroid
+	spawned.push_back(asteroid)
+	asteroid.camera = get_node(camera).get_path()
+	get_node(root).add_child(asteroid)
+	asteroid.global_transform.origin = global_transform.origin
