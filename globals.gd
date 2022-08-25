f8876b5b6f856025c098a5a5033f8d40d3edaf40
RichoM <richi.moran@gmail.com>
2022-08-25 18:11:19 -0300

    Moved the score handling to the globals script

--- globals.gd
+++ globals.gd
@@ -5,11 +5,14 @@ const LEADERBOARD_ID = "63062a5d8f40bba6d06f60be*KQQwDhbyYkW3wOtnkzx_2glfPQcl4Z0
 const LEADERBOARD_URL = SERVER_URL + LEADERBOARD_ID
 
 const USER_NAME_FILE_PATH = "user://user_name.paperairplane.bin"
+const SCORE_FILE_PATH = "user://max_score.paperairplane.bin"
 
 var user_name
+var max_score
 
 func _ready():
 	read_user_name()
+	read_max_score()
 	
 func read_user_name():
 	var file = File.new()
@@ -23,3 +26,16 @@ func set_user_name(s):
 	file.open(USER_NAME_FILE_PATH, File.WRITE)
 	file.store_string(user_name)
 	file.close()
+
+func read_max_score():
+	var score_file = File.new()
+	var error = score_file.open(SCORE_FILE_PATH, File.READ)
+	max_score = score_file.get_64()
+	score_file.close()
+
+func set_max_score(score):
+	max_score = score
+	var score_file = File.new()
+	score_file.open(SCORE_FILE_PATH, File.WRITE)
+	score_file.store_64(max_score)
+	score_file.close()
--- gui.gd
+++ gui.gd
@@ -1,21 +1,14 @@
 extends Control
 
 export(Array, String) var exclamation_messages
-const SCORE_FILE_PATH = "user://max_score.paperairplane.bin"
 
 var score = 0
-var max_score = 0
 
 var msg_counter = 0
 var game_over = false
 
 func _ready():
-	var score_file = File.new()
-	var error = score_file.open(SCORE_FILE_PATH, File.READ)
-	
-	max_score = score_file.get_64()
-	score_file.close()
-	
+	Globals.read_max_score()
 	$timer.connect("timeout", self, "update")
 	$timer.start(0.1)
 
@@ -31,7 +24,7 @@ func update():
 
 func update_score(s = 100):
 	score += s
-	
+	var max_score = Globals.max_score
 	$score_list/score.text = str(score)
 	$score_list/max_score.text = str(max_score)
 	if score > max_score:
@@ -41,17 +34,12 @@ func _input(event):
 	if Input.is_action_just_released("reset_max_score"):
 		update_max_score(0)
 
-
 func update_max_score(score):
-	max_score = score
-	var score_file = File.new()
-	score_file.open(SCORE_FILE_PATH, File.WRITE)
-	score_file.store_64(max_score)
-	score_file.close()
+	Globals.set_max_score(score)
 
 func _on_airplane_game_over():
 	game_over = true
-	if score > max_score:
+	if score > Globals.max_score:
 		update_max_score(score)
 
 func _on_airplane_bullet_time_score(score):
