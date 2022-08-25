48b78426d0aa289c0dee5f03bb31ab68bbfd9622
RichoM <richi.moran@gmail.com>
2022-08-25 18:27:09 -0300

    Renamed some scenes (for consistency)

--- menu.gd
+++ menu.gd
@@ -5,14 +5,14 @@ func _ready():
 
 func _on_new_game_pressed():
 	if Globals.user_name == "":
-		SceneManager.next_scene = "res://Main.tscn"
+		SceneManager.next_scene = "res://game.tscn"
 		SceneManager.change_scene("res://enter_name.tscn")
 	else:
-		SceneManager.change_scene("res://Main.tscn")
+		SceneManager.change_scene("res://game.tscn")
 
 func _on_leaderboard_pressed():
-	SceneManager.change_scene("res://Leaderboard.tscn")
+	SceneManager.change_scene("res://leaderboard.tscn")
 
 func _on_change_name_pressed():
-	SceneManager.next_scene = "res://Menu.tscn"
+	SceneManager.next_scene = "res://menu.tscn"
 	SceneManager.change_scene("res://enter_name.tscn")
