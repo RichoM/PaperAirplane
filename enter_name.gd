fb75af878e710fa729b4ef43f80f8b26d7e457ea
RichoM <richi.moran@gmail.com>
2022-08-26 09:04:32 -0300

    Fixing viewport and asteroid teleporting issue

--- enter_name.gd
+++ enter_name.gd
@@ -6,8 +6,14 @@ onready var player_name : LineEdit = get_node(player_name_path)
 func _ready():
 	if Globals.user_name != "":
 		player_name.placeholder_text = Globals.user_name
-
+	
 func _on_accept_button_pressed():
+	accept()
+
+func _on_player_name_text_entered(new_text):
+	accept()
+		
+func accept():
 	if player_name.text != "":
 		Globals.set_user_name(player_name.text)
 	else:
@@ -18,3 +24,4 @@ func _on_accept_button_pressed():
 
 func _on_back_button_pressed():
 	SceneManager.back()
+
